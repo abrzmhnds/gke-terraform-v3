a4690e89e7b5489b64828333ae4de9bb11c0cf52
DevOps <devops@DevOpss-MacBook-Pro.local>
2024-03-02 18:33:53 +0700

    add import

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+import {
+  id = "projects/gcp-shared-host-nonprod/global/networks/shared-host-nonprod"
+  to = google_compute_network.vpc
+}
+
+import {
+  id = "projects/gcp-shared-host-nonprod/regions/asia-southeast2/subnetworks/gcp-rnd-gke-node-devops"
+  to = google_compute_subnetwork.subnet
+}
+
 module "gkevpc" {
   source  = "app.terraform.io/cimb-tf-cloud/gkevpc/google"
   version = "1.0.1"
